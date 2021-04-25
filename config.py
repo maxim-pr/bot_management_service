c307b1ea84eb2bd82def9614052e70d5e540d009
Maxim Pryanikov <mx.pryanikov@gmail.com>
2021-04-26 02:03:00 +0300

    Add IP and PORT as environment variables

--- config.py
+++ config.py
@@ -10,3 +10,5 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 DB_URL = env.str("DB_URL")
 DB_NAME = env.str("DB_NAME")
 DB_USERS_COLLECTION_NAME = env.str("DB_USERS_COLLECTION_NAME")
+IP = env.str("IP")
+PORT = env.int("PORT")
--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import uvicorn
 from loader import app
 from routers import auth_router, bots_router
 
+from config import IP, PORT
+
 app.include_router(auth_router)
 app.include_router(bots_router)
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host=IP, port=PORT)
