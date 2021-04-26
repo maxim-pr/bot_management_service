c192341dc6c86af1f381258828e0c6001918f130
Maxim Pryanikov <mx.pryanikov@gmail.com>
2021-04-26 03:23:15 +0300

    Update Procfile

--- config.py
+++ config.py
@@ -10,5 +10,3 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 DB_URL = env.str("DB_URL")
 DB_NAME = env.str("DB_NAME")
 DB_USERS_COLLECTION_NAME = env.str("DB_USERS_COLLECTION_NAME")
-IP = env.str("IP")
-PORT = env.int("PORT")
--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-import uvicorn
-
 from loader import app
 from routers import auth_router, bots_router
 
-from config import IP, PORT
-
 app.include_router(auth_router)
 app.include_router(bots_router)
-
-
-if __name__ == "__main__":
-    uvicorn.run(app, host=IP, port=PORT)
